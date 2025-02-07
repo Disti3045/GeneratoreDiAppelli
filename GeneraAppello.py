f23ad9fa167b99797b5c2540b24b7e5448c51826
LeonardoDiStefano <165501974+Disti3045@users.noreply.github.com>
2025-02-08 00:55:16 +0100

    Update GeneraAppello.py

--- GeneraAppello.py
+++ GeneraAppello.py
@@ -4,7 +4,7 @@ Created on Fri Feb  7 13:21:28 2025
 @author: Leonardo Di Stefano
 """
 import os
-import PIL
+from PIL import Image
 import random
 ###############################################################################
 #Modifica le seguenti variabili in base alle tue necessità
@@ -15,23 +15,19 @@ quesitiTeoria = 3
 quesitiPratica = 5
 ###############################################################################
 pdf = list()
-totGen = len(os.listdir(pathOutput)))
-
+totGen = len(os.listdir(pathOutput))
+###############################################################################
 def DefinizioneImmagini(cartella):
-    #Estrazione dei file dalla cartella
-    estensioni = [".png", ".jpg", ".jpeg"]
-    listaFile = os.listdir(cartella)
-    #Rimozione elementi non validi dalla lista
-    for img in listaFile:
+    # Estrazione dei file dalla cartella
+    estensioni = (".png", ".jpg", ".jpeg")
+    listaFile = []
+    for img in os.listdir(cartella):
         if img.lower().endswith(estensioni):
             listaFile.append(os.path.join(cartella, img))
-            listaFile.remove(img)
-        else:
-            listaFile.remove(img)
     return listaFile
 def SelezioneQuesiti(maxLista, nQuesiti):
     indici = list()
-    for i in range(1, nQuesiti):
+    for i in range(0, nQuesiti):
         nmb = random.randint(0, maxLista-1)
         if nmb not in indici:
             indici.append(nmb)
@@ -40,17 +36,15 @@ def SelezioneQuesiti(maxLista, nQuesiti):
                 nmb = random.randint(0, maxLista-1)
             indici.append(nmb)
     return indici
-def EstrazionePercorsi(nmb, lista, listaFinale, totGen):
+def EstrazionePercorsi(nmb, lista, listaFinale):
     pdfList = list()
     for i in nmb:
         pdfList.append(lista[i])
     listaFinale += pdfList 
 def CreazionePdf(lista, totGen, pathUtente):
-    for img in lista:
-        img = PIL.Image.open(img).convert("RGB")
-        
-    lista[0].save(f"AppelloGenerato{totGen+1}", save_all=True, append_images=lista[1:])
+    immagini = [Image.open(img).convert("RGB") for img in lista]
     pdfPath = os.path.join(pathUtente, f"AppelloGenerato{totGen+1}.pdf")
+    immagini[0].save(pdfPath, save_all=True, append_images=immagini[1:])
     return pdfPath
 ###############################################################################
 listaTeoria = DefinizioneImmagini(teoria)
@@ -65,5 +59,4 @@ if len(nmbT)>1 and len(nmbP)>1:
     EstrazionePercorsi(nmbP, listaPratica, pdf)
 else:
     print("Invalid numbers")
-
-os.open(CreazionePdf(pdf, totGen, pathOutput))
+os.startfile(CreazionePdf(pdf, totGen, pathOutput))
