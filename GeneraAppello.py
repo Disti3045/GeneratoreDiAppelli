c7aae9f033dc6bc72c672dbc8257de3fa2c4d7b5
LeonardoDiStefano <165501974+Disti3045@users.noreply.github.com>
2025-02-08 00:38:01 +0100

    Update GeneraAppello.py

--- GeneraAppello.py
+++ GeneraAppello.py
@@ -6,12 +6,17 @@ Created on Fri Feb  7 13:21:28 2025
 import os
 import PIL
 import random
-
-teoria = os.path.expanduser("~\OneDrive\Desktop\GeneratoreDiAppelli\Teoria")
-pratica = os.path.expanduser("~\OneDrive\Desktop\GeneratoreDiAppelli\Pratica")
-totGen = len(os.listdir(os.path.expanduser("~\OneDrive\Desktop\GeneratoreDiAppelli\Appelli")))
-pdf = list()
 ###############################################################################
+#Modifica le seguenti variabili in base alle tue necessità
+teoria = os.path.expanduser("~\Desktop\GeneratoreDiAppelli\Teoria")
+pratica = os.path.expanduser("~\Desktop\GeneratoreDiAppelli\Pratica")
+pathOutput = os.path.expanduser("~\Desktop\GeneratoreDiAppelli\Appelli")
+quesitiTeoria = 3
+quesitiPratica = 5
+###############################################################################
+pdf = list()
+totGen = len(os.listdir(pathOutput)))
+
 def DefinizioneImmagini(cartella):
     #Estrazione dei file dalla cartella
     estensioni = [".png", ".jpg", ".jpeg"]
@@ -24,13 +29,8 @@ def DefinizioneImmagini(cartella):
         else:
             listaFile.remove(img)
     return listaFile
-def SelezioneQuesiti(maxLista, tipo):
+def SelezioneQuesiti(maxLista, nQuesiti):
     indici = list()
-    if tipo == "Teoria":
-        nQuesiti=3
-    else:
-        nQuesiti=5
-    #Estrazioe Quesiti
     for i in range(1, nQuesiti):
         nmb = random.randint(0, maxLista-1)
         if nmb not in indici:
@@ -45,19 +45,19 @@ def EstrazionePercorsi(nmb, lista, listaFinale, totGen):
     for i in nmb:
         pdfList.append(lista[i])
     listaFinale += pdfList 
-def CreazionePdf(lista, totGen):
+def CreazionePdf(lista, totGen, pathUtente):
     for img in lista:
         img = PIL.Image.open(img).convert("RGB")
         
     lista[0].save(f"AppelloGenerato{totGen+1}", save_all=True, append_images=lista[1:])
-    pdfPath = os.path.join(os.path.expanduser("~\OneDrive\Desktop\GeneratoreDiAppelli\Appelli"), f"AppelloGenerato{totGen+1}.pdf")
+    pdfPath = os.path.join(pathUtente, f"AppelloGenerato{totGen+1}.pdf")
     return pdfPath
 ###############################################################################
 listaTeoria = DefinizioneImmagini(teoria)
 listaPratica = DefinizioneImmagini(pratica)
 if len(listaTeoria)>3 and len(listaPratica)>5:
-    nmbT = SelezioneQuesiti(len(listaTeoria), "Teoria")
-    nmbP = SelezioneQuesiti(len(listaPratica), "Pratica")
+    nmbT = SelezioneQuesiti(len(listaTeoria), quesitiTeoria)
+    nmbP = SelezioneQuesiti(len(listaPratica), quesitiPratica)
 else:
     print("Invalid elements")
 if len(nmbT)>1 and len(nmbP)>1:
@@ -66,4 +66,4 @@ if len(nmbT)>1 and len(nmbP)>1:
 else:
     print("Invalid numbers")
 
-os.open(CreazionePdf(pdf, totGen))
+os.open(CreazionePdf(pdf, totGen, pathOutput))
